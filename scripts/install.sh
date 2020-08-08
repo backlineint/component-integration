6682d030f036f80955d0a6f8516223aee18502ae
Brian Perry <contact.brianperry@gmail.com>
2020-08-08 10:31:32 -0500

    Core updates

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,8 @@
 lando start
 lando composer install
 lando db-import seed.sql.gz
+lando drush updb
+# enable default content (probabably can just add this config)
 lando drush cim sync --no-interaction
 lando drush cr
 cd web/themes/custom/nes/nes-components/
